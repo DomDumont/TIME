44f735ea589b6347b6668367f8833ed3dd8fe8d8
Dominique Dumont <dom.dumont@gmail.com>
2015-02-24 21:39:34 +0100

    Update cmake_generic.sh

---
+++ cmake_generic.sh
@@ -0,0 +1,12 @@
+
+# Determine source tree and build tree
+if [ "$1" ] && [[ ! "$1" =~ ^- ]]; then BUILD=$1; shift; elif [ -f $(pwd)/CMakeCache.txt ]; then BUILD=$(pwd); else echo An error has occured, build tree has to be provided as the first argument OR call this script in a build tree itself; exit 1; fi
+SOURCE=$(dirname $0)
+if [ "$SOURCE" == "." ]; then SOURCE=$(pwd); fi
+if [ "$BUILD" == "." ]; then BUILD=$(pwd); fi
+
+
+
+
+# Create project with the chosen CMake generator and toolchain
+cmake -E make_directory "$BUILD" && cmake -E chdir "$BUILD" cmake $OPTS $@ "$SOURCE" && post_cmake
