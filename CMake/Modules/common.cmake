dd436599e02c6791bf4a85e1bc0419409ab9355f
MeyerAtWork <meyer.dumont@gmail.com>
2015-03-02 16:10:42 +0100

    Plein de conneries, maintenant que ça marche, il faut tout casser sinon c'est pas drôle.

--- CMake/Modules/common.cmake
+++ CMake/Modules/common.cmake
@@ -6,41 +6,42 @@ macro (check_source_files)
     endif ()
 endmacro ()
 
+if (LINUX)
+
+     if (TE_64BITS)
+            set_property (GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS TRUE)
+        else()
+            set_property (GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS FALSE)
+        endif ()
 
 
- if (TE_64BITS)
-        set_property (GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS TRUE)
-    else()
-        set_property (GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS FALSE)
+    set (CMAKE_BUILD_TYPE Debug)
+
+
+    # execute_process (COMMAND echo COMMAND ${CMAKE_C_COMPILER} -E -dM - OUTPUT_VARIABLE PREDEFINED_MACROS ERROR_QUIET)
+    # string (REGEX MATCH "#define +__(x86_64|aarch64)__ +1" matched "${PREDEFINED_MACROS}")
+    # if (matched)
+    # set (TE_64BITS TRUE)
+    # else ()
+    # set (TE_64BITS FALSE)
+    # endif ()
+
+    set (TE_64BITS FALSE)
+
+     if (TE_64BITS)
+    	message ("wouesh on passe en 64 bits !!!")
+    	set (DASH_MBIT -m64) # This variable is intentionally not defined on Android and RPI platform
+    else ()
+    	message ("wouesh on passe en 33 bits !!!")
+    	set (DASH_MBIT -m32)
     endif ()
 
+    set (CMAKE_C_FLAGS_DEBUG  "${CMAKE_C_FLAGS_DEBUG} ${DASH_MBIT}")
+    message("ici => ${CMAKE_C_FLAGS_DEBUG}")
+    set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${DASH_MBIT}")
+    message("ici => ${CMAKE_CXX_FLAGS_DEBUG}")
+    set (CMAKE_SHARED_LINKER_FLAGS_DEBUG "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} ${DASH_MBIT}")
+    set (CCMAKE_EXE_LINKER_FLAGS_DEBUG "${CCMAKE_EXE_LINKER_FLAGS_DEBUG} ${DASH_MBIT}")
+    set (CMAKE_MODULE_LINKER_FLAGS_DEBUG "${CMAKE_MODULE_LINKER_FLAGS_DEBUG} ${DASH_MBIT}")
 
-set (CMAKE_BUILD_TYPE Debug)
-
-
-# execute_process (COMMAND echo COMMAND ${CMAKE_C_COMPILER} -E -dM - OUTPUT_VARIABLE PREDEFINED_MACROS ERROR_QUIET)
-# string (REGEX MATCH "#define +__(x86_64|aarch64)__ +1" matched "${PREDEFINED_MACROS}")
-# if (matched)
-# set (TE_64BITS TRUE)
-# else ()
-# set (TE_64BITS FALSE)
-# endif ()
-
-set (TE_64BITS FALSE)
-
- if (TE_64BITS)
-	message ("wouesh on passe en 64 bits !!!")
-	set (DASH_MBIT -m64) # This variable is intentionally not defined on Android and RPI platform
-else ()
-	message ("wouesh on passe en 33 bits !!!")
-	set (DASH_MBIT -m32)
-endif ()
-
-set (CMAKE_C_FLAGS_DEBUG  "${CMAKE_C_FLAGS_DEBUG} ${DASH_MBIT}")
-message("ici => ${CMAKE_C_FLAGS_DEBUG}")
-set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${DASH_MBIT}")
-message("ici => ${CMAKE_CXX_FLAGS_DEBUG}")
-set (CMAKE_SHARED_LINKER_FLAGS_DEBUG "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} ${DASH_MBIT}")
-set (CCMAKE_EXE_LINKER_FLAGS_DEBUG "${CCMAKE_EXE_LINKER_FLAGS_DEBUG} ${DASH_MBIT}")
-set (CMAKE_MODULE_LINKER_FLAGS_DEBUG "${CMAKE_MODULE_LINKER_FLAGS_DEBUG} ${DASH_MBIT}")
-
+endif()
--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -13,6 +13,164 @@ void OnNativeWinMouseMove(int mousex, int mousey, bool lbutton)
 {
 }
 
+class RenderState
+	{
+		public:
+		RenderState() : po(0), vertLoc(0), mvpLoc(0), normalLoc(0), texcoordLoc(0), texUnitLoc(0)
+			{
+			}
+		~RenderState() {}
+
+		GLint po;
+		GLint vertLoc;
+		GLint mvpLoc;
+		GLint lightLoc;
+		GLint normalLoc;
+		GLint texcoordLoc;
+		GLint texUnitLoc;
+
+		GLfloat yaw;
+		GLfloat pitch;
+
+
+	};
+
+class esContext
+	{
+		public:
+		esContext() :
+			nativeDisplay(0), nativeWin(0),
+			eglDisplay(0), eglSurface(0), eglContext(0),
+			nWindowWidth(0), nWindowHeight(0), nMouseX(0), nMouseY(0)
+			{
+			}
+
+		~esContext() {}
+
+		EGLNativeDisplayType nativeDisplay;
+		EGLNativeWindowType nativeWin;
+		EGLDisplay eglDisplay;
+		EGLSurface eglSurface;
+		EGLContext eglContext;
+
+		int         nWindowWidth;
+		int         nWindowHeight;
+		int         nMouseX;
+		int         nMouseY;
+
+		RenderState rs;
+	};
+
+
+EGLBoolean Setup(esContext &ctx)
+	{
+	EGLBoolean bsuccess;
+
+	// create native window
+	EGLNativeDisplayType nativeDisplay;
+	if (!OpenNativeDisplay(&nativeDisplay))
+		{
+		printf("Could not get open native display\n");
+		return GL_FALSE;
+		}
+
+	// get egl display handle
+	EGLDisplay eglDisplay;
+	eglDisplay = eglGetDisplay(nativeDisplay);
+	if (eglDisplay == EGL_NO_DISPLAY)
+		{
+		printf("Could not get EGL display\n");
+		CloseNativeDisplay(nativeDisplay);
+		return GL_FALSE;
+		}
+	ctx.eglDisplay = eglDisplay;
+
+	// Initialize the display
+	EGLint major = 0;
+	EGLint minor = 0;
+	bsuccess = eglInitialize(eglDisplay, &major, &minor);
+	if (!bsuccess)
+		{
+		printf("Could not initialize EGL display\n");
+		CloseNativeDisplay(nativeDisplay);
+		return GL_FALSE;
+		}
+	if (major < 1 || minor < 4)
+		{
+		// Does not support EGL 1.4
+		printf("System does not support at least EGL 1.4\n");
+		CloseNativeDisplay(nativeDisplay);
+		return GL_FALSE;
+		}
+
+	// Obtain the first configuration with a depth buffer
+	EGLint attrs[] = { EGL_DEPTH_SIZE, 16, EGL_NONE };
+	EGLint numConfig = 0;
+	EGLConfig eglConfig = 0;
+	bsuccess = eglChooseConfig(eglDisplay, attrs, &eglConfig, 1, &numConfig);
+	if (!bsuccess)
+		{
+		printf("Could not find valid EGL config\n");
+		CloseNativeDisplay(nativeDisplay);
+		return GL_FALSE;
+		}
+
+	// Get the native visual id
+	int nativeVid;
+	if (!eglGetConfigAttrib(eglDisplay, eglConfig, EGL_NATIVE_VISUAL_ID, &nativeVid))
+		{
+		printf("Could not get native visual id\n");
+		CloseNativeDisplay(nativeDisplay);
+		return GL_FALSE;
+		}
+
+	EGLNativeWindowType nativeWin;
+	if (!CreateNativeWin(nativeDisplay, 640, 480, nativeVid, &nativeWin))
+		{
+		printf("Could not create window\n");
+		CloseNativeDisplay(nativeDisplay);
+		return GL_FALSE;
+		}
+
+	// Create a surface for the main window
+	EGLSurface eglSurface;
+	eglSurface = eglCreateWindowSurface(eglDisplay, eglConfig, nativeWin, NULL);
+	if (eglSurface == EGL_NO_SURFACE)
+		{
+		printf("Could not create EGL surface\n");
+		DestroyNativeWin(nativeDisplay, nativeWin);
+		CloseNativeDisplay(nativeDisplay);
+		return GL_FALSE;
+		}
+	ctx.eglSurface = eglSurface;
+
+	// Create an OpenGL ES context
+	EGLContext eglContext;
+	eglContext = eglCreateContext(eglDisplay, eglConfig, EGL_NO_CONTEXT, NULL);
+	if (eglContext == EGL_NO_CONTEXT)
+		{
+		printf("Could not create EGL context\n");
+		DestroyNativeWin(nativeDisplay, nativeWin);
+		CloseNativeDisplay(nativeDisplay);
+		return GL_FALSE;
+		}
+
+	// Make the context and surface current
+	bsuccess = eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext);
+	if (!bsuccess)
+		{
+		printf("Could not activate EGL context\n");
+		DestroyNativeWin(nativeDisplay, nativeWin);
+		CloseNativeDisplay(nativeDisplay);
+		return GL_FALSE;
+		}
+
+	ctx.nativeDisplay = nativeDisplay;
+	ctx.nativeWin = nativeWin;
+	return GL_TRUE;
+	}
+
+
 int main()
 {
   EGLBoolean bsuccess;
